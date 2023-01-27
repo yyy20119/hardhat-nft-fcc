c382897516fb76049f7f187b3c4e9f9b0959f93c
yyy20119 <55304848+yyy20119@users.noreply.github.com>
2023-01-27 23:06:30 +0800

    Update 04-mint.js

--- deploy/04-mint.js
+++ deploy/04-mint.js
@@ -5,10 +5,10 @@ module.exports = async ({ getNamedAccounts }) => {
     const chainId = network.config.chainId
 
     // Basic NFT
-    const basicNft = await ethers.getContract("BasicNft", deployer)
-    const basicMintTx = await basicNft.mintNft()
-    await basicMintTx.wait(1)
-    console.log(`Basic NFT index 0 tokenURI: ${await basicNft.tokenURI(0)}`)
+    // const basicNft = await ethers.getContract("BasicNft", deployer)
+    // const basicMintTx = await basicNft.mintNft()
+    // await basicMintTx.wait(1)
+    // console.log(`Basic NFT index 0 tokenURI: ${await basicNft.tokenURI(0)}`)
 
     // Dynamic SVG  NFT
     const highValue = ethers.utils.parseEther("2000")
@@ -18,23 +18,23 @@ module.exports = async ({ getNamedAccounts }) => {
     console.log(`Dynamic SVG NFT index 0 tokenURI: ${await dynamicSvgNft.tokenURI(0)}`)
 
     // Random IPFS NFT
-    const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
-    const mintFee = await randomIpfsNft.getMintFee()
-    const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() })
-    const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1)
-    // Need to listen for response
-    await new Promise(async (resolve, reject) => {
-        setTimeout(() => reject("Timeout: 'NFTMinted' event did not fire"), 300000) // 5 minute timeout time
-        // setup listener for our event
-        randomIpfsNft.once("NftMinted", async () => {
-            resolve()
-        })
-        if (chainId == 31337) {
-            const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString()
-            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
-            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
-        }
-    })
-    console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`)
+    // const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
+    // const mintFee = await randomIpfsNft.getMintFee()
+    // const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() })
+    // const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1)
+    // // Need to listen for response
+    // await new Promise(async (resolve, reject) => {
+    //     setTimeout(() => reject("Timeout: 'NFTMinted' event did not fire"), 300000) // 5 minute timeout time
+    //     // setup listener for our event
+    //     randomIpfsNft.once("NftMinted", async () => {
+    //         resolve()
+    //     })
+    //     if (chainId == 31337) {
+    //         const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString()
+    //         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+    //         await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
+    //     }
+    // })
+    // console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`)
 }
 module.exports.tags = ["all", "mint"]
